5b418d84d121c569d26fe9fe25309888fdb86fa1
Micronik-PS <micronik.ps@outlook.com>
2025-08-09 17:29:08 +0300

    Создан проект из шаблона MFC

---
+++ Installer/Installer.h
@@ -0,0 +1,32 @@
+﻿
+// Installer.h: главный файл заголовка для приложения PROJECT_NAME
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "включить pch.h до включения этого файла в PCH"
+#endif
+
+#include "resource.h"		// основные символы
+
+
+// CInstallerApp:
+// Сведения о реализации этого класса: Installer.cpp
+//
+
+class CInstallerApp : public CWinApp
+{
+public:
+	CInstallerApp();
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+
+// Реализация
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CInstallerApp theApp;
---
+++ Installer/InstallerDlg.h
@@ -0,0 +1,33 @@
+﻿
+// InstallerDlg.h: файл заголовка
+//
+
+#pragma once
+
+
+// Диалоговое окно CInstallerDlg
+class CInstallerDlg : public CDialogEx
+{
+// Создание
+public:
+	CInstallerDlg(CWnd* pParent = nullptr);	// стандартный конструктор
+
+// Данные диалогового окна
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_INSTALLER_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// поддержка DDX/DDV
+
+
+// Реализация
+protected:
+	HICON m_hIcon;
+
+	// Созданные функции схемы сообщений
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
