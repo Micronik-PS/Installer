d27b8423f238d1ccbc8140ca28068152efdfa05d
Micronik-PS <micronik.ps@outlook.com>
2025-08-10 13:35:34 +0300

    Изменение формата лого приложений с BMP на PNG

--- Installer/Resource.h
+++ Installer/Resource.h
@@ -4,8 +4,6 @@
 //
 #define IDR_MAINFRAME                   128
 #define IDD_DIALOG_WELCOME_PAGE         129
-#define IDB_BITMAP_XNVIEW_LOGO          131
-#define IDB_BITMAP_ZIP_LOGO             132
 
 // Next default values for new objects
 // 
@@ -13,7 +11,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        133
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
